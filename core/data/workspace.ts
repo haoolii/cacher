05c701afabe87c1f5df36ffeafe79f7920e1ac5f
Hao <unnhao@gmail.com>
2024-07-09 17:30:51 +0800

    feat: test case still have some problem

--- core/data/workspace.ts
+++ core/data/workspace.ts
@@ -4,48 +4,51 @@ import utc from "dayjs/plugin/utc";
 
 dayjs.extend(utc);
 
-export const getQueryDateRange = (date: string) => {
-  const localDate = dayjs(date);
+// Step1. Get local date start and end.
+// Step2. Transfer local date start and end to UTC+0 it will be a range.
+// Step3. Use that range add or minus millisecond. it will be a new range.
+// Step4. Query date which is include in range.
+export const getWorksByDateRange = (dateRange: string[]) => {
+  if (dateRange.length !== 2) throw new Error("wrong range");
+  const [start, end] = dateRange;
+
+  return getWorks().filter(
+    (work) => dayjs(work.date).isAfter(start) && dayjs(work.date).isBefore(end)
+  );
+};
+
+export const getLocalDateToISORange = (date: string) => {
+  let localDate = dayjs(date);
   const startOfDay = localDate.startOf("day").toISOString();
   const endOfDay = localDate.endOf("day").toISOString();
   return [startOfDay, endOfDay];
 };
 
-export const getQueryWorksByDate = (date: string) => {
-  const localDate = dayjs(date);
-  const startOfDay = localDate.startOf("day").toISOString();
-  const endOfDay = localDate.endOf("day").toISOString();
-
-  return getWorks().filter((work) => {
-    const workDate = dayjs(work.date);
-    return (
-      workDate.isAfter(dayjs(startOfDay).subtract(1, "second")) &&
-      workDate.isBefore(dayjs(endOfDay).add(1, "second"))
-    );
-  });
-};
-export const getQueryReviewConfigs = () => {
-  return getReviewConfigs();
+export const dateRangeCalculate = (
+  dateRange: string[],
+  millisecond: number
+) => {
+  if (dateRange.length !== 2) throw new Error("wrong range");
+  const [start, end] = dateRange;
+  const newStart = dayjs(start).add(millisecond, "milliseconds").toISOString();
+  const newEnd = dayjs(end).add(millisecond, "milliseconds").toISOString();
+  return [newStart, newEnd];
 };
 
-export const getQueryReviewDatesByDate = (date: string) => {
-  const reviewConfigs = getReviewConfigs();
-  const localDate = dayjs(date);
-  const startOfDay = localDate.startOf("day").toISOString();
-  const endOfDay = localDate.endOf("day").toISOString();
-
-  const result: string[] = [];
-  console.log('local', dayjs(date).toISOString());
-  reviewConfigs.forEach((config) => {
-    result.push(
-      dayjs(date)
-        .utc()
-        .add(-config.duration, "milliseconds")
-        .startOf("day")
-        .toString()
-        // .toISOString()
-    );
-  });
-
+export const getWorksByLocalDateIncludeReviewConfig = (localDate: string) => {
+  const configs = getReviewConfigs();
+  const [start, end] = getLocalDateToISORange(localDate);
+  const result = [];
+  for (const config of configs) {
+    const [tStart, tEnd] = dateRangeCalculate([start, end], -config.duration);
+    const works = getWorksByDateRange([tStart, tEnd]);
+    result.push({
+      start: tStart,
+      end: tEnd,
+      local_start: dayjs(tStart).format("YYYY-MM-DD HH:mm:ss"),
+      local_end: dayjs(tEnd).format("YYYY-MM-DD HH:mm:ss"),
+      works: works,
+    });
+  }
   return result;
 };
